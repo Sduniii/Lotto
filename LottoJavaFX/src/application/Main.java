93f9a508179e9fc70cecca22d6621041581e4b2e
Tobias Sdun <tobias.sdun@campus.tu-berlin.de>
2015-08-16 22:11:13 +0200

    Gui ready

--- LottoJavaFX/src/application/Main.java
+++ LottoJavaFX/src/application/Main.java
@@ -1,22 +1,32 @@
 package application;
 	
 
-import java.util.ArrayList;
-
 import components.MouseBox;
 import javafx.application.Application;
-import javafx.geometry.Point2D;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class Main extends Application{
 	
 	Stage window;
-	Scene sceneOne, sceneTwo;
+	Scene sceneOne;
+	double xOffset, yOffset;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -24,7 +34,6 @@ public class Main extends Application{
 			
 			initStage();
 			initSceneOne();
-			initSceneTwo();
 			
 			
 			
@@ -42,42 +51,91 @@ public class Main extends Application{
 	
 	public void initStage(){
 		this.window.setTitle("Lotto");
+		this.window.initStyle(StageStyle.TRANSPARENT);
 	}
 	
 	private void initSceneOne() {
-		VBox root = new VBox(10);
+		BorderPane root = new BorderPane();
+		root.getStyleClass().add("main-style");
 		
-		//Button
-		Button button = new Button("Los");
-		button.setOnAction(e -> {
-			this.window.setScene(this.sceneTwo);
+		//Top Menu
+		HBox menu = new HBox();
+		menu.setPrefSize(this.window.getWidth(), 0);
+		menu.setPadding(new Insets(2, 2, 2, 10));
+		menu.setSpacing(10);
+		menu.getStyleClass().add("hbox-top");
+		menu.setOnMousePressed(event -> {
+			xOffset = this.window.getX() - event.getScreenX();
+            yOffset = this.window.getY() - event.getScreenY();
 		});
-		Button button2 = new Button("Los2");
-		button2.setOnAction(e -> {
-			ArrayList<Point2D> list = MouseBox.display("Mouse");
-			System.out.println(list.size());
+		menu.setOnMouseDragged(event -> {
+			this.window.setX(event.getScreenX() + xOffset);
+			this.window.setY(event.getScreenY() + yOffset);
 		});
 		
-		root.getChildren().addAll(button, button2);
+		Button closeButton = new Button("X");
+		closeButton.setOnAction(e -> this.window.close());
+		closeButton.getStyleClass().add("close-button");
 		
-		this.sceneOne = new Scene(root,400,400);
-		this.sceneOne.getStylesheets().add(getClass().getResource("application.css").toExternalForm());		
-	}
-
-	private void initSceneTwo() {
-		VBox root = new VBox(20);
+		Label name =  new Label("LottoZahlen by Sduniii");
+		name.setId("label-name");
+		final ImageView logo = new ImageView();
+        Image image1 = new Image(getClass().getResource("../assets/logo.png").toExternalForm(), 20, 20, false, false);
+        logo.setImage(image1);
+		menu.getChildren().addAll(logo, name);
 		
-		//Button
-		Button button = new Button("Zurück");
-		button.setOnAction(e -> {
-			this.window.setScene(this.sceneOne);
-		});
-		Label label = new Label("Yeah");
+		StackPane stack = new StackPane();
+		stack.getChildren().add(closeButton);
+		stack.setAlignment(Pos.CENTER_RIGHT);
+		HBox.setHgrow(stack, Priority.ALWAYS);
 		
-		root.getChildren().addAll(button,label);
+		menu.getChildren().add(stack);
 		
-		this.sceneTwo = new Scene(root,200,200);
-		this.sceneTwo.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		root.setTop(menu);
+		//END Top Menu
+		
+		
+		
+		GridPane contentPane = new GridPane();
+		contentPane.setPadding(new Insets(10, 10, 10, 10));
+		contentPane.setVgap(8);
+		contentPane.setHgap(10);
+		
+		Label labelNormal = new Label("6 aus 49 Felder: ");
+		GridPane.setConstraints(labelNormal, 0, 0);
+		
+		TextField textFieldNormal = new TextField();
+		textFieldNormal.setPromptText("4");
+		GridPane.setConstraints(textFieldNormal, 1, 0);
+		
+		Label labelEuro = new Label("Euro Jackpot Felder: ");
+		GridPane.setConstraints(labelEuro, 0, 1);
+		
+		TextField textFieldEuro = new TextField();
+		textFieldEuro.setPromptText("4");
+		GridPane.setConstraints(textFieldEuro, 1, 1);
+		
+		Button startButton = new Button("Los");
+		startButton.setOnAction(e -> MouseBox.display("Bewege die Maus!"));
+		GridPane.setConstraints(startButton, 3, 1);
+		
+		contentPane.getChildren().addAll(labelNormal,textFieldNormal,labelEuro,textFieldEuro, startButton);
+		
+		root.setCenter(contentPane);
+		
+		
+		//TextArea
+		StackPane consolePane = new StackPane();
+		TextArea console = new TextArea();
+		console.setEditable(false);
+		consolePane.getChildren().add(console);
+		
+		root.setBottom(consolePane);
+		
+		this.sceneOne = new Scene(root);
+		this.sceneOne.setFill(Color.TRANSPARENT);
+		
+		this.sceneOne.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		
 	}
 
--- LottoJavaFX/src/components/MouseBox.java
+++ LottoJavaFX/src/components/MouseBox.java
@@ -2,32 +2,69 @@ package components;
 
 import java.util.ArrayList;
 
+import application.Main;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MouseBox implements EventHandler<MouseEvent> {
 
 	public static ArrayList<Point2D> display(String title){
+		final BooleanProperty shiftPressed = new SimpleBooleanProperty(false);
+		final DoubleProperty xOffset = new SimpleDoubleProperty();
+		final DoubleProperty yOffset = new SimpleDoubleProperty();
 		Stage window = new Stage();
-		
+		window.initStyle(StageStyle.TRANSPARENT);
 		ArrayList<Point2D> list = new ArrayList<>();
 		window.initModality(Modality.APPLICATION_MODAL);
-		window.setTitle(title);
+		
+		BorderPane root = new BorderPane();
+		root.getStyleClass().add("main-style");
+		root.setOnMousePressed(event -> {
+			xOffset.set(window.getX() - event.getScreenX());
+            yOffset.set(window.getY() - event.getScreenY());
+		});
+		root.setOnMouseDragged(event -> {
+			window.setX(event.getScreenX() + xOffset.get());
+			window.setY(event.getScreenY() + yOffset.get());
+		});
+		
 		AnchorPane pane = new AnchorPane();
-			
+		root.setCenter(pane);
 	
-		Scene scene = new Scene(pane,100,100);
+		Scene scene = new Scene(root,200,200);
+		scene.setOnKeyPressed(e->{
+			if(shiftPressed.getValue() && e.getCode() == KeyCode.X){
+				window.close();
+			}else if(e.getCode() == KeyCode.SHIFT){
+				shiftPressed.setValue(true);
+			}
+		});
+		scene.setOnKeyReleased(e -> {
+			if(e.getCode() == KeyCode.SHIFT){
+				shiftPressed.setValue(false);
+			}
+		});
+		
+		scene.setFill(Color.TRANSPARENT);
+		scene.getStylesheets().add(Main.class.getResource("application.css").toExternalForm());
 		scene.setOnMouseMoved(e -> {
 			Rectangle rect = new Rectangle(e.getX(), e.getY(), 1, 1);
 			list.add(new Point2D(rect.getX(), rect.getY()));
-			rect.setFill(Color.BLACK);
+			rect.getStyleClass().add("rect");
 			pane.getChildren().add(rect);
 		});
 		window.setScene(scene);
